a6562ae4b91fc61ee521e8ee6d273821f9aeb56f
1BObC <bob.cocksedge@gmail.com>
2022-03-11 15:55:16 +0000

    Files set up and ready to compile

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Contracts
 VS Code Solidity code generation DEMO from https://docs.nethereum.com/en/latest/nethereum-codegen-vscode-demo/
+"Files set up and ready to compile"
---
+++ SimpleStorage.sol
@@ -0,0 +1,16 @@
+//from nethereum tutorial
+//will be ready to check solidity version 
+
+pragma solidity >=0.4.0 <0.7.0;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
